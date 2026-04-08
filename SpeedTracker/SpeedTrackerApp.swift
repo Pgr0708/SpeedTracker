afe0608aa73c44acbd203668c3da411bed08965f
Pgr0708 <parthgohil0708@gmail.com>
2026-04-08 15:47:21 +0530

    Initial Commit

---
+++ SpeedTracker/SpeedTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedTrackerApp.swift
+//  SpeedTracker
+//
+//  Created by Minaxi on 08/04/26.
+//
+
+import SwiftUI
+
+@main
+struct SpeedTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
